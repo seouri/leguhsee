1754291176c0423609559a6a2c315e893092fc32
Joon Lee <seouri@gmail.com>
2012-02-16 12:33:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leguhsee::Application.config.secret_token = '126757f18ea6ef86a3dd91638e9b065b270af81857d713b98fad3150a026cf09f3540f855c7a7b0c80813598cd79507fec698c5001d2e6e58cb413df3e3a46db'
